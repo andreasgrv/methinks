44d7457a6f04817959724ef8bc78d3ef0f35fec9
Andreas Grivas <andreasgrv@gmail.com>
2019-11-11 23:02:34 +0000

    Make database creation possible

--- web/app.py
+++ web/app.py
@@ -4,20 +4,26 @@ from flask import Flask, request, render_template, render_template_string
 from flask_migrate import Migrate
 from methinks.db import db
 
-# create our application :)
-app = Flask(__name__)
-
-# global options
-app.config['DEBUG'] = True
 
 db_uri = 'postgres://%s:%s@localhost/%s' % (os.environ['DB_USER'],
                                             os.environ['DB_PASSWD'],
                                             os.environ['DB_NAME'])
-app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
-migrate = Migrate(app, db)
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['DEBUG'] = True
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    # global options
+    return app
+
+
+app = create_app()
 
 
 @app.route('/')
